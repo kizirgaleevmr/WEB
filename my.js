461ea665f1d6374d6c1753272f8fc3483418043e
marsel <mkrmarsel@gmail.com>
2023-12-15 14:55:56 +0500

    last OSI

---
+++ my.js
@@ -0,0 +1,46 @@
+// let myNumber = prompt('Введите число')
+
+// while(isNaN(myNumber) || myNumber == null){
+//     alert('Вы ввели не число')
+//     myNumber = prompt('Введите число')
+// }
+
+// console.log(myNumber)
+
+// if(myNumber < 0 ){
+//     console.log(`Число отрицательное: ${myNumber}`)
+// } else{
+//     console.log(`Число положительное: ${myNumber}`)
+// }
+
+// let prom = new Promise((resolve,reject)=>{
+//    setTimeout(()=> resolve(myNumber),2000)
+// })
+// .then(data =>{
+//     console.log(data)
+// })
+
+// let inText = document.querySelector('.inText')
+
+// inText.addEventListener('change', (el)=>{
+//     el = el.target
+//     console.log(el.value);
+
+//     let htElem = document.createElement('h1')
+//     htElem.className = 'inText'
+//     htElem.innerHTML = el.value;
+//     document.body.append(htElem);
+// })
+// const mas = {
+//     'one' : 4,
+//     'two' : 33,
+//     'name one' : 'Marsel',
+//     'age' : 38,
+//     'bool' : true
+// }
+
+// let out = document.getElementById('out');
+
+// for(let key in mas){
+//     out.innerHTML += key + ' --- ' + mas[key] + '<br>';
+// }
